c3bb016a0b7efded9dcd1e73a647e259c1fe158b
Connor Burri <connorjburri@gmail.com>
2019-12-18 21:07:56 -0500

    Changed Scope
    changed the scope to allow for more information to be read about the user

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email ugc-image-upload playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
